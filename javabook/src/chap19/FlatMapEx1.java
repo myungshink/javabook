1d0e8afd649edb0c8260b411889cc5135e018bf2
gdj2-1 <gdj2-1@DESKTOP-LITE9MO>
2019-01-18 18:37:34 +0900

    stream 20190118-2

--- javabook/src/chap19/FlatMapEx1.java
+++ javabook/src/chap19/FlatMapEx1.java
@@ -5,14 +5,11 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class FlatMapEx1 {
-
 	public static void main(String[] args) {
 		List<String> list1 = Arrays.asList("자바 8버전에 ", "추가된 스트림을","열심히 공부하자");
 		list1.stream().flatMap(data -> Arrays.stream(data.split(" ")))
 			.forEach(word -> System.out.println(word));
-		
 		System.out.println();
-		
 		List<String> list2 = Arrays.asList("10, 20, 30", "40, 50, 60");
 		IntStream isr = list2.stream().flatMapToInt(data -> {
 				String[] strArr = data.split(",");
